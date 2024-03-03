bcf4839bf81825e7075ab74836273056bc853ef4
ahenfoa <izanrry@gmail.com>
2024-03-03 16:16:35 +0000

    100-print_tebahpla.py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,4 +1,3 @@
-#!/usr/bin/python3
-for c in range(ord('z'), ord('A') - 1, -1):
-    print("{:c}".format(c if c % 2 == 1 else c - 32), end='')
+for c in range(ord('z'), ord('a') - 1, -2):
+    print(chr(c) + chr(c - 33), end='')
 
